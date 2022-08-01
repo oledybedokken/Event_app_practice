a4352893e8bb9f926122c6e08ef61318e8067911
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2022-08-01 22:33:57 +0200

    Fix

--- event_app/pages/index.js
+++ event_app/pages/index.js
@@ -3,16 +3,20 @@ import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
-
-
   return (
     <div className={styles.container}>
       <Head>
-        <h1>Prototype 1</h1>
+        <title>Favspot | Home</title>
+        <link rel="icon" href="/favspot.ico" />
       </Head>
       <main className={styles.main}>
+        <h1>
+          Welcome
+        </h1>
+        <h2>
+          Gather all your friends with on click!
+        </h2>
       </main>
-
     </div>
   )
 
